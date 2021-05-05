5bc7f3443e5f7485b19ef28bb50e5b7081e193d1
nikitha26 <mnikitha365@gmail.com>
2021-05-05 16:17:12 +0530

    api call

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Axios from 'axios';
+function App() {
+  // const City = async() => {
+  //   const response = await Axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=588&date=05-05-2021&pincode=503122')
+  //     console.log(response.data.sessions[0])
+  //     var one = response.data.sessions[0];
+  //     console.log(one.district_name)
+  // }
+  //   City();
+     return (
+    <div>
+      <h1>Hai </h1>
+    </div>
+  );
+}
+
+export default App;
