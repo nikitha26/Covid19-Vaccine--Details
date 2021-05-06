b9b11de7d64f2cdabab5282eabf48d576fa91668
nikitha26 <mnikitha365@gmail.com>
2021-05-06 22:50:01 +0530

    reactstrap added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
+import {Container,Row,Col,Form,Input,Button} from 'reactstrap'
 import Axios from 'axios';
 function App() {
-  // const City = async() => {
-  //   const response = await Axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=588&date=05-05-2021&pincode=503122')
-  //     console.log(response.data.sessions[0])
-  //     var one = response.data.sessions[0];
-  //     console.log(one.district_name)
-  // }
-  //   City();
+   
+  const City = async() => {
+    //  var dist = document.getElementById("district").value;
+    //  console.log(dist)
+    var date = new Date();
+    console.log(date.getDate() +"  "+date.getMonth())
+    const response = await Axios.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=588&date=06-05-2021&pincode=503122')
+      console.log(response.data)
+      // var one = response.data.sessions[0];
+      // console.log(one)
+  }
+    City();
      return (
-    <div>
-      <h1>Hai </h1>
-    </div>
+    <Container className="mt-5 pt-5">
+      <Row className="mt-5">
+        <Col className="col-lg-6 col-md-6 col-sm-12 offset-lg-3">
+          <Input
+            type="text"
+            placeholder="Enter Mondal"
+            id="district"
+          >
+          </Input>
+          <br/>
+          <Button
+           color="primary"
+          >View By District</Button>  
+        </Col>
+      </Row>   
+    </Container>  
   );
 }
 
